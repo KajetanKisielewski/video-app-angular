d882bb927376a680e989bd0a25277b98fea5e690
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2023-03-09 13:03:43 +0100

    searchBar component added

--- video-app-angular/src/app/home/home.module.ts
+++ video-app-angular/src/app/home/home.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { HomeComponent } from './home.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
@@ -10,7 +12,9 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
     SearchBarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [
     HomeComponent,
